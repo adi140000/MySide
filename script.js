25bec3d1b52815065bfec66551e9ca1e42e6377f
Adrian <adi120.a@gmail.com>
2018-11-05 02:04:28 +0100

    Upgrade logo and nav and  added new items

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const icons =  document.querySelector(".icons");
 const mainNav = document.querySelector("nav.main");
+
+const logo = document.querySelector("div.main");
 let ifs=true;
 
+let decision=true
+// nav sticky
+
+
 const fixdMenu =function(e){
-    if(window.scrollY>200){
+    if(window.innerWidth>768){
+        console.log("yes");
+        if(window.scrollY>mainNav.offsetTop){
         mainNav.classList.add("active");
+        const width = mainNav.offsetHeight;
+        if(decision)
+        {
+        $("nav.main").css("top",-width);
+        $("nav.main").animate({top:0},700);
+        decision=false;
+        }
+        
+        
     }
-    else{
+     if(window.scrollY<logo.offsetHeight){
         mainNav.classList.remove("active");
+        decision=true;
+
     }
+}else{
+    
 }
+}
+    
 
-window.addEventListener("scroll" ,fixdMenu)
-
+window.addEventListener("scroll" ,fixdMenu);
+setInterval(fixdMenu,10);
 
 
 
