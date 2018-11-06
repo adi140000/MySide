780df9254e9fa22d82089ecb719f98044f72bda9
Adrian <adi120.a@gmail.com>
2018-11-06 02:23:29 +0100

    added new items

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const icons =  document.querySelector(".icons");
 const mainNav = document.querySelector("nav.main");
-
+const arrow = document.querySelector("div.arrow");
 const logo = document.querySelector("div.main");
-let ifs=true;
+
 
 let decision=true
 // nav sticky
@@ -10,17 +10,17 @@ let decision=true
 
 const fixdMenu =function(e){
     if(window.innerWidth>768){
-        console.log("yes");
-        if(window.scrollY>mainNav.offsetTop){
-        mainNav.classList.add("active");
-        const width = mainNav.offsetHeight;
-        if(decision)
-        {
-        $("nav.main").css("top",-width);
-        $("nav.main").animate({top:0},700);
-        decision=false;
-        }
         
+        if(window.scrollY>mainNav.offsetTop){
+            mainNav.classList.add("active");
+            const width = mainNav.offsetHeight;
+            if(decision)
+            {
+                $("nav.main").css("top",-width);
+                $("nav.main").animate({top:0},700);
+                decision=false;
+            
+            }
         
     }
      if(window.scrollY<logo.offsetHeight){
@@ -29,27 +29,48 @@ const fixdMenu =function(e){
 
     }
 }else{
-    
+    mainNav.classList.remove("active");
 }
 }
-    
+
+const showArrow=function(){
+
+    if(window.scrollY>window.innerHeight){
+        arrow.style.display="block";
+        arrow.addEventListener("click",function(){
+            $("div.arrow").fadeOut();    
+        })
+    }else
+    {
+        arrow.style.display="none"; 
+    }
+}
 
 window.addEventListener("scroll" ,fixdMenu);
-setInterval(fixdMenu,10);
 
+window.addEventListener("scroll",showArrow);
 
+window.addEventListener("mousemove",function(e){
+document.cr
+})
 
 
 //battery
-let i=1;
-const changesIcons = function(){
-if(i>4){
-    i=1;
+const battery = function(){
+
+    let i=1;
+    const changesIcons = function(){
+        if(i>4){
+            i=1;
+        }
+
+        icons.className=`icon-bat${i}`;
+        i++
+
+        }
+    return changesIcons
 }
 
-icons.className=`icon-bat${i}`;
-i++
+const changesIcons = battery();
 
-}
-
-setInterval(changesIcons , 1000)
+setInterval(changesIcons , 1000);
